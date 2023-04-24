e72b6f49f96f52ca70bbce6425b92f0ce1a585b4
Andy Jackson <andypauljackson@gmail.com>
2023-04-24 11:39:53 +0100

    removing duplicates

--- index.js
+++ index.js
@@ -16,7 +16,9 @@ function getEmotionsArray(cats){
     const emotionsArray = [];
     for (let cat of cats){ 
         for (let emotion of cat.emotionTags) {
+            if (!emotionsArray.includes(emotion)){
             emotionsArray.push(emotion);
+            }
         }
     }
     console.log(emotionsArray)
