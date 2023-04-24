2e59b2b861eda8c73653ddb449eacb03c5cddb78
Andy Jackson <andypauljackson@gmail.com>
2023-04-24 11:06:53 +0100

    emotion added as radio button.  CSS updated

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
-import { catsData } from './data.js';
+import { catsData } from '/data.js';
+
 const emotionRadios = document.getElementById('emotion-radios');
 
 function getEmotionsArray(cats){
@@ -27,9 +28,20 @@ function renderEmotionsRadios(cats){
     let radioItems = ``;
     const emotions = getEmotionsArray(cats);
     for (let emotion of emotions){
-        radioItems += `<p>${emotion}</p>`
+        radioItems += `
+        <div class="radio">
+            <label for="${emotion}">${emotion}</label>
+            <input
+                type="radio" 
+                id="${emotion}"
+                value="${emotion}"
+                name="emotions"
+            >
+        </div>
+    `
     }
     emotionRadios.innerHTML = radioItems;
+
 }
 renderEmotionsRadios(catsData);
 
