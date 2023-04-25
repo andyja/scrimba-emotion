9bb36effdfc86ff4ddb4a475486c61b4925cbfce
Andy Jackson <andypauljackson@gmail.com>
2023-04-25 10:17:05 +0100

    refactor

--- index.js
+++ index.js
@@ -1,50 +1,49 @@
+import { catsData } from '/data.js'
 
-import { catsData } from '/data.js';
+const emotionRadios = document.getElementById('emotion-radios')
 
-const emotionRadios = document.getElementById('emotion-radios');
+emotionRadios.addEventListener('change', highlightCheckedOption)
+
+function highlightCheckedOption(e){
+    const radios = document.getElementsByClassName('radio')
+    
+    for (let radio of radios){
+        radio.classList.remove('highlight')
+    }
+
+    document.getElementById(e.target.id).parentElement.classList.add('highlight')
+}
 
 function getEmotionsArray(cats){
-    // let emotionsArray = [];
-    // cats.forEach(cat => { 
-    //     cat.emotionTags.forEach(emotion => {
-    //         if(!emotionsArray.includes(emotion)){
-    //             emotionsArray.push(emotion);
-    //         }
-    //     })
-    // });   
-    // console.log(emotionsArray)
-    const emotionsArray = [];
-    for (let cat of cats){ 
-        for (let emotion of cat.emotionTags) {
+    const emotionsArray = []    
+    for (let cat of cats){
+        for (let emotion of cat.emotionTags){
             if (!emotionsArray.includes(emotion)){
-            emotionsArray.push(emotion);
+                emotionsArray.push(emotion)
             }
         }
     }
-    console.log(emotionsArray)
-    return emotionsArray;
-    
+    return emotionsArray
 }
 
+
 function renderEmotionsRadios(cats){
-    let radioItems = ``;
-    const emotions = getEmotionsArray(cats);
+        
+    let radioItems = ``
+    const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
         radioItems += `
         <div class="radio">
             <label for="${emotion}">${emotion}</label>
             <input
-                type="radio" 
-                id="${emotion}"
-                value="${emotion}"
-                name="emotions"
+            type="radio"
+            id="${emotion}"
+            value="${emotion}"
+            name="emotions"
             >
-        </div>
-    `
+        </div>`
     }
-    emotionRadios.innerHTML = radioItems;
-
+    emotionRadios.innerHTML = radioItems
 }
-renderEmotionsRadios(catsData);
 
-
+renderEmotionsRadios(catsData)
