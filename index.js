d074692cd504d6e5a73334870ea1ec6fdce5fcc4
Andy Jackson <andypauljackson@gmail.com>
2023-05-02 10:34:47 +0100

    refactor

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ import { catsData } from '/data.js';
 
 const emotionRadios = document.getElementById('emotion-radios');
 const getImageBtn = document.getElementById('get-image-btn');
-const gifsOnlyOption = document.getElementById("gifs-only-option")
+const gifsOnlyOption = document.getElementById("gifs-only-option");
+const memeModalInner = document.getElementById("meme-modal-inner");
+const memeModal = document.getElementById("meme-modal");
+const memeModalCloseBtn = document.getElementById("meme-modal-close-btn");
 
 emotionRadios.addEventListener('change', highlightCheckedOption)
-
-getImageBtn.addEventListener('click', getMatchingCatsArray);
+memeModalCloseBtn.addEventListener('click', closeModal);
+getImageBtn.addEventListener('click', renderCat);
 
 function highlightCheckedOption(e){
     const radios = document.getElementsByClassName('radio')
@@ -16,19 +19,51 @@ function highlightCheckedOption(e){
     document.getElementById(e.target.id).parentElement.classList.add('highlight')
 }
 
+function closeModal(){
+    memeModal.style.display = 'none';
+}
+function renderCat(){
+    const catObject = getSingleCatObject()
+    memeModal.ineerHTML = `
+    <img
+    class="cat-img"
+    src="./images/${catObject.image}"
+    alt="${catObject.alt}"
+    >
+   `
+   memeModal.style.display = 'flex';
+}
+function getSingleCatObject(){
+    const catsArray = getMatchingCatsArray()
+    
+    if (catsArray.length === 1){
+        return (catsArray[0])
+    }
+    else {
+        const ranNum = Math.floor(Math.random() * catsArray.length)
+        return (catsArray[ranNum]);
+    }
+}
 function getMatchingCatsArray(){
     if (document.querySelector('input[type="radio"]:checked')){
         const selectedEmotion = document.querySelector('input[type="radio"]:checked').value;
         const isGif = gifsOnlyOption.checked;
         
         const matchingCatsArray = catsData.filter(function(cat){
-            return cat.emotionTags.includes(selectedEmotion)
+            if (isGif){
+                return cat.emotionTags.includes(selectedEmotion) && cat.isGif
+
+            }
+            else{
+                return cat.emotionTags.includes(selectedEmotion)
+            }
+
+            
         })
 
-        console.log(matchingCatsArray);
+        return matchingCatsArray;
     }
 }
-
 function getEmotionsArray(cats){
     const emotionsArray = []    
     for (let cat of cats){
@@ -40,10 +75,7 @@ function getEmotionsArray(cats){
     }
     return emotionsArray
 }
-
-
 function renderEmotionsRadios(cats){
-        
     let radioItems = ``
     const emotions = getEmotionsArray(cats)
     for (let emotion of emotions){
